ccbaf425a70ef4653e3021a39fda822f6983921d
Tohirbek04 <tohirbekakhmedov1310@gmail.com>
2024-05-25 00:09:53 +0500

    static and media files

--- root/settings.py
+++ root/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
@@ -117,8 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+MEDIA_URL = os.path.join('media/')
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
 
-STATIC_URL = 'static/'
+STATIC_URL = os.path.join('static/')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static/')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
